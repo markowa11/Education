09e9a59ed2d87bb7508aa7b98c69e979c99a43cf
Markowa11 <114814646+markowa11@users.noreply.github.com>
2022-11-07 14:20:50 +0200

    commit

--- uDemy/02-Fundamentals-Part-2/starter/codingChallanges.js
+++ uDemy/02-Fundamentals-Part-2/starter/codingChallanges.js
@@ -19,17 +19,33 @@
 
 // checkWinner(scoreDolphins, scoreKoalas)
 
-function checkWinner() {
-    let avgDolphins = (300 + 23 + 71) / 3
-    let avgKoalas = (65 + 54 + 49) / 3
+// function checkWinner() {
+//     let avgDolphins = (300 + 23 + 71) / 3
+//     let avgKoalas = (65 + 54 + 49) / 3
 
-    if (avgDolphins >= 2 * avgKoalas) {
-        console.log(`Dolphins win (${avgDolphins.toFixed(0)} vs ${avgKoalas})`)
-    } else if (avgKoalas >= 2 * avgDolphins) {
-        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+//     if (avgDolphins >= 2 * avgKoalas) {
+//         console.log(`Dolphins win (${avgDolphins.toFixed(0)} vs ${avgKoalas})`)
+//     } else if (avgKoalas >= 2 * avgDolphins) {
+//         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+//     } else {
+//         console.log(`No team wins`)
+//     }
+// }
+
+// checkWinner()
+
+// Coding Challenge #2
+
+function calculator(input) {
+    let bill = Number(input[0])
+    let tip = 0
+
+    if (bill >= 50 && bill <= 500) {
+        tip = bill * 0.15
     } else {
-        console.log(`No team wins`)
+        tip = bill * 0.20
     }
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
 }
 
-checkWinner()
+calculator([125])
